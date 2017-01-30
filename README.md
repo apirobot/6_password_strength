5eaf9a4a1f938d16059dcd72c01c9b22c135fe84
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-01-30 14:03:05 +0300

    Added solution and README update.

--- README.md
+++ README.md
@@ -1,7 +1,44 @@
-# Password Strength Calculator
+## Password Strength Calculator
 
-[TODO. There will be project description]
+`6_password_strength` is a password estimator that gives score from 1 to 10.
 
-# Project Goals
+### Requirements
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+* [zxcvbn-python](https://github.com/dwolfhub/zxcvbn-python)
+
+### Installation
+
+1. [Download](https://github.com/apirobot/6_password_strength/archive/master.zip) 6_password_strength from GitHub;
+2. Install dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+### Usage
+
+```
+usage: password_strength.py [-h] [-i INPUTS [INPUTS ...]]
+
+Password Strength Calculator
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUTS [INPUTS ...], --inputs INPUTS [INPUTS ...]
+                        list of user inputs like name and email
+```
+
+### Usage Examples
+
+```bash
+python password_strength.py
+>>> Type password: correcthorsebatterystaple
+>>> Password strength: 10 of 10
+
+python password_strength.py
+>>> Type password: DenisPa$$w0rd
+>>> Password strength: 5 of 10
+
+python password_strength.py --inputs Denis denis@gmail.com
+>>> Type password: DenisPa$$w0rd
+>>> Password strength: 3 of 10
+```
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,72 @@
+import argparse
+from getpass import getpass
+
+from zxcvbn import zxcvbn
+
+parser = argparse.ArgumentParser(description='Password Strength Calculator')
+parser.add_argument('-i', '--inputs', nargs='+', type=str,
+                    help='list of user inputs like name and email')
+args = parser.parse_args()
 
 
-def get_password_strength(password):
-    pass
+def input_password():
+    password = getpass(prompt='Input password: ')
+    while ' ' in password:
+        password = getpass(prompt='Spaces are not allowed.'
+                                  ' Input correct password: ')
+    return password
+
+
+def get_password_strength(password, user_inputs=[]):
+    """
+    Gets password strength considering ``user_inputs``. Since ``zxcvbn``
+    function returns score from 0 to 4 and ``get_password_strength`` should
+    return score from 1 to 10, therefore ``guesses_to_score`` comes into play.
+
+    :param user_inputs: List of strings like name or email. ``password`` that
+                        includes a user's personal information can be heavily
+                        penalized.
+    :returns: Password strength from 1 to 10.
+    """
+    result = zxcvbn(password, user_inputs)
+
+    def guesses_to_score(guesses):
+        delta = 5
+
+        if guesses < 1e3 + delta:
+            return 1
+        elif guesses < 1e4 + delta:
+            return 2
+        elif guesses < 1e5 + delta:
+            return 3
+        elif guesses < 1e6 + delta:
+            return 4
+        elif guesses < 1e7 + delta:
+            return 5
+        elif guesses < 1e8 + delta:
+            return 6
+        elif guesses < 1e9 + delta:
+            return 7
+        elif guesses < 1e10 + delta:
+            return 8
+        elif guesses < 1e11 + delta:
+            return 9
+        else:
+            return 10
+
+    result['score'] = guesses_to_score(result['guesses'])
+    return result['score']
+
+
+def main():
+    password = input_password()
+    if args.inputs is not None:
+        password_strength = get_password_strength(password, args.inputs)
+    else:
+        password_strength = get_password_strength(password)
+
+    print('Password strength: %d of 10' % password_strength)
 
 
 if __name__ == '__main__':
-    pass
+    main()
