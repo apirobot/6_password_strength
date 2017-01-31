f077185b845a6c185152cbebda393182b9a2c2fd
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-01-31 10:17:42 +0300

    Refactor

--- password_strength.py
+++ password_strength.py
@@ -19,42 +19,43 @@ def input_password():
 
 def get_password_strength(password, user_inputs=[]):
     """
-    Gets password strength considering ``user_inputs``. Since ``zxcvbn``
-    function returns score from 0 to 4 and ``get_password_strength`` should
-    return score from 1 to 10, therefore ``guesses_to_score`` comes into play.
+    Gets password strength considering ``user_inputs``.
 
     :param user_inputs: List of strings like name or email. ``password`` that
                         includes a user's personal information can be heavily
                         penalized.
+    :param guesses: How many guesses need to iterate through
+                    to crack a password.
     :returns: Password strength from 1 to 10.
     """
     result = zxcvbn(password, user_inputs)
 
-    def guesses_to_score(guesses):
-        delta = 5
+    # Since ``zxcvbn`` function returns score from 0 to 4 and
+    # ``get_password_strength`` should return score from 1 to 10,
+    # therefore result['score'] should be changed.
+    delta = 5
+    guesses = result['guesses']
+    if guesses < 1e3 + delta:
+        result['score'] = 1
+    elif guesses < 1e4 + delta:
+        result['score'] = 2
+    elif guesses < 1e5 + delta:
+        result['score'] = 3
+    elif guesses < 1e6 + delta:
+        result['score'] = 4
+    elif guesses < 1e7 + delta:
+        result['score'] = 5
+    elif guesses < 1e8 + delta:
+        result['score'] = 6
+    elif guesses < 1e9 + delta:
+        result['score'] = 7
+    elif guesses < 1e10 + delta:
+        result['score'] = 8
+    elif guesses < 1e11 + delta:
+        result['score'] = 9
+    else:
+        result['score'] = 10
 
-        if guesses < 1e3 + delta:
-            return 1
-        elif guesses < 1e4 + delta:
-            return 2
-        elif guesses < 1e5 + delta:
-            return 3
-        elif guesses < 1e6 + delta:
-            return 4
-        elif guesses < 1e7 + delta:
-            return 5
-        elif guesses < 1e8 + delta:
-            return 6
-        elif guesses < 1e9 + delta:
-            return 7
-        elif guesses < 1e10 + delta:
-            return 8
-        elif guesses < 1e11 + delta:
-            return 9
-        else:
-            return 10
-
-    result['score'] = guesses_to_score(result['guesses'])
     return result['score']
 
 
